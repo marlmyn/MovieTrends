79e94aac1bd8ad5a2843e1714bebf9164a5f510c
Akmaral <e.akmaral024@mail.ru>
2024-01-26 16:19:25 +0600

    Initial Commit

---
+++ MovieTrends/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MovieTrends
+//
+//  Created by Akmaral Ergesh on 26.01.2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        return true
+    }
+
+}
+
---
+++ MovieTrends/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MovieTrends
+//
+//  Created by Akmaral Ergesh on 26.01.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    //MARK: IBOutlets:
+    @IBOutlet var tableView: UITableView!
+    
+    //MARK: ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configView()
+    }
+
+    func configView() {
+        self.title = "Main View"
+        self.view.backgroundColor = .systemCyan
+        
+        setupTableView()
+    }
+
+}
