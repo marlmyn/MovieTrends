061ec510ab8480724afda30c90c28d77d7d4d971
Akmaral <e.akmaral024@mail.ru>
2024-02-04 13:01:09 +0600

    Add Networking & Model

---
+++ MovieTrends/Model/TrendingMoviesModel.swift
@@ -0,0 +1,8 @@
+//
+//  TrendingMoviesModel.swift
+//  MovieTrends
+//
+//  Created by Akmaral Ergesh on 01.02.2024.
+//
+
+import Foundation
---
+++ MovieTrends/Networking/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  MovieTrends
+//
+//  Created by Akmaral Ergesh on 01.02.2024.
+//
+
+import Foundation
